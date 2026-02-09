0d8315c77a8a57e2675c170c005b3762af0af791
Radhouen911 <162017878+Radhouen911@users.noreply.github.com>
2026-02-09 19:51:20 +0100

    Change base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // GitHub Pages base path for project repo
+  base: "/", // GitHub Pages base path for project repo
 });
